fc54406c8311f0930aa1b3e671e99dfd5012e44b
Kenny Li <kenny@Kennys-MacBook-Pro.local>
2017-02-24 18:23:33 +0800

    Update the port for heroku testing environment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   console.log('listening on *:3000');
 });
 
