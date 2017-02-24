680c92e6ac8f67db5af0e114085a11b177236309
Kenny Li <kenny@Kennys-MacBook-Pro.local>
2017-02-24 18:03:18 +0800

    Initial POC on socket-io P2P implementation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express();
 
 console.log("Current Directory", __dirname);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function (req, res) {
   //res.send('<h1>Hello world</h1>'); 
@@ -18,11 +19,27 @@ var server = app.listen(3000, function () {
 });
 
 var io = require('socket.io')(server);
+var p2pserver = require('socket.io-p2p-server').Server
+
+//If we want to enable p2p signalling to all clients
+io.use(p2pserver);
 io.on('connection', function (socket) {
+
+  console.log("User connected (", socket.id, ") ");
+
+  //if we only want to enable the p2p signalling to a particular room
+  //then we have to enable it here 
+  //p2pserver(socket, null, room_name);
+
+  //to join a room
+  socket.on('room', function (room) {
+    console.log("Room ", room, "joined");
+    socket.join(room);
+  });
 
   //Trigger when message received  
   socket.on('message', function (messageBundle) {
-    console.log(messageBundle.username, messageBundle.message);
+    console.log("(", messageBundle.username, ")", messageBundle.message);
     socket.broadcast.emit('receive_message', messageBundle);
   });
 
@@ -30,6 +47,4 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     console.log('User disconnected');
   })
-
-
 });
